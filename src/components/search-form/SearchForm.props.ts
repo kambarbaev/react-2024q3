d0a52f23b252b5ce631bf59ed8fd0f2eb6b9195f
Talgat Kambarbaev <akamypka@gmail.com>
2024-07-07 12:34:56 +0600

    feat: add set and get search string in local storage

---
+++ src/components/search-form/SearchForm.props.ts
@@ -0,0 +1,3 @@
+export type SearchFormState = {
+  searchString: string;
+};
--- src/components/search-input/SearchInput.tsx
+++ src/components/search-input/SearchInput.tsx
@@ -1,9 +1,14 @@
-import { Component } from 'react';
+import { Component, ChangeEvent } from 'react';
 import styles from './SearchInput.module.css';
+import { SearchInputProps } from './SearchInput.props';
 
-class SearchInput extends Component {
+class SearchInput extends Component<SearchInputProps> {
+  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.props.onChange(event.target.value);
+  };
+
   render() {
-    return <input className={styles['input']} />;
+    return <input className={styles['input']} type="text" value={this.props.value} onChange={this.handleChange} />;
   }
 }
 
