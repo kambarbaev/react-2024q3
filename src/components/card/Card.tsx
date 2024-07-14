10a90ad64712731e59be8303842db55a17c5eb34
Talgat Kambarbaev <akamypka@gmail.com>
2024-07-14 16:32:44 +0600

    feat: update main list styles

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -5,27 +5,6 @@ function Card({ person }: CardProps) {
   return (
     <li className={style['card']}>
       <h2 className={style['name']}>{person.name}</h2>
-      <div className={style['row']}>
-        Height: <span>{person.height}</span>
-      </div>
-      <div className={style['row']}>
-        Mass: <span>{person.mass}</span>
-      </div>
-      <div className={style['row']}>
-        Hair Color: <span>{person.hair_color}</span>
-      </div>
-      <div className={style['row']}>
-        Skin Color: <span>{person.skin_color}</span>
-      </div>
-      <div className={style['row']}>
-        Eye Color: <span>{person.eye_color}</span>
-      </div>
-      <div className={style['row']}>
-        Birth Year: <span>{person.birth_year}</span>
-      </div>
-      <div className={style['row']}>
-        Gender: <span>{person.gender}</span>
-      </div>
     </li>
   );
 }
