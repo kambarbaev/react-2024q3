c34d0617d7b6aa57bc1cef87383198f9d00b9eed
Talgat Kambarbaev <akamypka@gmail.com>
2024-07-18 00:13:13 +0600

    feat: add check error for wrong query page or search

--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -7,16 +7,22 @@ import { People } from '../../serviсes/api.props';
 function Details() {
   const [person, setPerson] = useState<People | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
   const { id } = useParams();
 
   useEffect(() => {
-    console.log('id', id);
     if (id) {
       const personUrl = `${id}`;
-      fetchPerson(personUrl).then((data: People) => {
-        setPerson(data);
-        setLoading(false);
-      });
+      fetchPerson(personUrl)
+        .then((data: People) => {
+          setPerson(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setError(true);
+          setLoading(false);
+        });
     }
   }, [id]);
 
@@ -24,10 +30,13 @@ function Details() {
     <div className={styles['details']}>
       {loading ? (
         <div>Loading...</div>
+      ) : error ? (
+        <div>No available data </div>
       ) : (
         person && (
           <>
             <h2>{person.name}</h2>
+            {/* Добавьте другие свойства персонажа, если нужно */}
           </>
         )
       )}
