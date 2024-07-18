8d6e31f61e01631851e4433dd08bc3bcee67cafa
Talgat Kambarbaev <akamypka@gmail.com>
2024-07-18 11:26:41 +0600

    feat: implement open and close details when click on item in list search results

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,19 +1,40 @@
 import { useNavigate, useLocation } from 'react-router-dom';
-import style from './Card.module.css';
 import { CardProps } from './Card.props';
 import { getPersonIdFromUrl } from '../../utils/getPersonIdFromUrl';
+import style from './Card.module.css';
+import { useEffect, useState } from 'react';
 
 function Card({ person, currentPage }: CardProps) {
+  const [open, setOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   const location = useLocation();
   const id = getPersonIdFromUrl(person.url);
 
-  const handleClick = () => {
-    if (location.pathname.includes('search')) {
-      const keyword = location.pathname.split('/')[2];
-      navigate(`/search/${keyword}/page/${currentPage}/${id}`);
+  useEffect(() => {
+    if (location.pathname.includes(id)) {
+      setOpen(true);
     } else {
-      navigate(`/people/page/${currentPage}/${id}`);
+      setOpen(false);
+    }
+  }, [id, location.pathname, open]);
+
+  const handleClick = () => {
+    if (!open) {
+      setOpen(true);
+      if (location.pathname.includes('search')) {
+        const keyword = location.pathname.split('/')[2];
+        navigate(`/search/${keyword}/page/${currentPage}/${id}`);
+      } else {
+        navigate(`/people/page/${currentPage}/${id}`);
+      }
+    } else {
+      setOpen(false);
+      if (location.pathname.includes('search')) {
+        const keyword = location.pathname.split('/')[2];
+        navigate(`/search/${keyword}/page/${currentPage}`);
+      } else {
+        navigate(`/people/page/${currentPage}`);
+      }
     }
   };
 
