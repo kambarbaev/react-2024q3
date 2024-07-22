194db880c2449f1208144a886e405a7fd794b761
Talgat Kambarbaev <akamypka@gmail.com>
2024-07-22 14:33:03 +0600

    feat: add vitest in project, create button test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -15,6 +16,21 @@ export default defineConfig({
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@router': path.resolve(__dirname, './src/router'),
       '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/utils/setusTests.ts',
+    globals: true,
+    coverage: {
+      include: ['src'],
+      exclude: ['**/index.ts', '**/main.tsx', '**/App.tsx'],
+      thresholds: {
+        lines: 50,
+        functions: 50,
+        branches: 50,
+        statements: 50,
+      },
     },
   },
 });
